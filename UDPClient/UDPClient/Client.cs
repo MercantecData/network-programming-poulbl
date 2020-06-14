4d95f1e916bdf94a17b4fc45089dc70159857473
Poul <poulbachlauritsen@gmail.com>
2020-06-14 13:19:39 +0200

    Barebones UDP "client"
    Den kan ikke rigtigt noget udover at skrive en besked til localhost.

---
+++ UDPClient/UDPClient/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UDPClient
+{
+    class Client
+    {
+        public Client()
+        {
+            UdpClient client = new UdpClient();
+
+            string text = "Hello UDP D:";
+            byte[] bytes = Encoding.UTF8.GetBytes(text);
+
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1243);
+            Send(client, bytes, endpoint);
+        }
+
+        public async void Send(UdpClient client, byte[] bytes, IPEndPoint endpoint)
+        {
+            await client.SendAsync(bytes, bytes.Length, endpoint);
+        }
+
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+        }
+    }
+}
